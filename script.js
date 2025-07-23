a1727f78dc057e51aaa0f259a4b725ce0cf7ed47
Ismoil-21 <toxirovi82@gmail.com>
2025-07-23 20:52:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+function func(son, name, price) {
+  let number = prompt(
+    `${name} dan nechta zakaz qilmohchisiz?`
+);
+
+switch(number){
+    case "1":
+        let zakaz = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break;
+    case "2":
+        let as = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break;  
+    case "3":
+        let asa = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break;
+    case "4":
+        let aww = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break;
+    case "5":
+        let arr = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break; 
+    case "6":
+        let ar = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break; 
+    case "7":
+        let aaa = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break; 
+    case "8":
+        let aa = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break; 
+    case "9":
+        let asd = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break; 
+    case "10":
+        let ass = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break; 
+    case "11":
+        let az = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break; 
+    case "12":
+        let asad = confirm(`${number} ta ${name} jami narxi ${price * number} so'm \n
+            Maxsulotni zakaz qilasizmi?`);
+            alert("Zakazingiz qabul qilindi!!!");
+        break;   
+}
+
+}
+
